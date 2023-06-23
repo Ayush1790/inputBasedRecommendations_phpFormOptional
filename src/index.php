ce592cb9d91b4df8c04ba455ec01df613d8b9fd7
Ayush1790 <ayushgupta@cedcoss.com>
2023-06-23 11:14:55 +0530

    done

---
+++ src/index.php
@@ -0,0 +1,10 @@
+<!-- form to enter age and weight from the user -->
+<form>
+    <p style="color:brown;"> Enter Age between 5-20</p>
+    <span>Enter Age</span><input type="number" id="age">
+    <span>Enter Your Weight</span><input type="number" id="weight">
+    <input type="button" value="Submit" onclick="checkWeight();">
+    <p id="p" style="color:red;"></p>
+</form>
+
+<script src="./script.js"></script>
